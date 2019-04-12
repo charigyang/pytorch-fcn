35a77b629d0dceb0b46f788c73727fc696422aef
= <=>
2019-04-12 11:20:12 +0100

    debug

--- torchfcn/models/unet.py
+++ torchfcn/models/unet.py
@@ -61,7 +61,7 @@ class up_conv(nn.Module):
 
 class UNet(nn.Module):
     def __init__(self,img_ch=3,output_ch=1):
-        super(U_Net,self).__init__()
+        super(UNet,self).__init__()
         
         self.Maxpool = nn.MaxPool2d(kernel_size=2,stride=2)
 
