ac6b228b48e0c35a6b591dbb30492672312f4910
= <=>
2019-04-12 15:33:22 +0100

    test

--- examples/voc/unet.py
+++ examples/voc/unet.py
@@ -2,8 +2,10 @@ import os.path as osp
 
 import fcn
 import numpy as np
+
 import torch
 import torch.nn as nn
+import torch.nn.functional as functional
 
 def init_weights(net, init_type='normal', gain=0.02):
     def init_func(m):
@@ -28,100 +30,94 @@ def init_weights(net, init_type='normal', gain=0.02):
     print('initialize network with %s' % init_type)
     net.apply(init_func)
 
+
+def add_conv_stage(dim_in, dim_out, kernel_size=3, stride=1, padding=1, bias=True, useBN=False):
+  if useBN:
+    return nn.Sequential(
+      nn.Conv2d(dim_in, dim_out, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias),
+      nn.BatchNorm2d(dim_out),
+      nn.LeakyReLU(0.1),
+      nn.Conv2d(dim_out, dim_out, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias),
+      nn.BatchNorm2d(dim_out),
+      nn.LeakyReLU(0.1)
+    )
+  else:
+    return nn.Sequential(
+      nn.Conv2d(dim_in, dim_out, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias),
+      nn.ReLU(),
+      nn.Conv2d(dim_out, dim_out, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias),
+      nn.ReLU()
+    )
+
+def add_merge_stage(ch_coarse, ch_fine, in_coarse, in_fine, upsample):
+  conv = nn.ConvTranspose2d(ch_coarse, ch_fine, 4, 2, 1, bias=False)
+  torch.cat(conv, in_fine)
+
+  return nn.Sequential(
+    nn.ConvTranspose2d(ch_coarse, ch_fine, 4, 2, 1, bias=False)
+  )
+  upsample(in_coarse)
+
+def upsample(ch_coarse, ch_fine):
+  return nn.Sequential(
+    nn.ConvTranspose2d(ch_coarse, ch_fine, 4, 2, 1, bias=False),
+    nn.ReLU()
+  )
+
 class UNet(nn.Module):
-    def __init__(self,img_ch=3,output_ch=21):
-        super(UNet, self).__init__()
-        
-        self.Maxpool = nn.MaxPool2d(kernel_size=2,stride=2)
+  def __init__(self, useBN=True):
+    super(UNet, self).__init__()
 
-        self.Conv1 = conv_block(ch_in=img_ch,ch_out=64)
-        self.Conv2 = conv_block(ch_in=64,ch_out=128)
-        self.Conv3 = conv_block(ch_in=128,ch_out=256)
-        self.Conv4 = conv_block(ch_in=256,ch_out=512)
-        self.Conv5 = conv_block(ch_in=512,ch_out=1024)
+    self.conv1   = add_conv_stage(1, 32, useBN=useBN)
+    self.conv2   = add_conv_stage(32, 64, useBN=useBN)
+    self.conv3   = add_conv_stage(64, 128, useBN=useBN)
+    self.conv4   = add_conv_stage(128, 256, useBN=useBN)
+    self.conv5   = add_conv_stage(256, 512, useBN=useBN)
 
-        self.Up5 = up_conv(ch_in=1024,ch_out=512)
-        self.Up_conv5 = conv_block(ch_in=1024, ch_out=512)
+    self.conv4m = add_conv_stage(512, 256, useBN=useBN)
+    self.conv3m = add_conv_stage(256, 128, useBN=useBN)
+    self.conv2m = add_conv_stage(128,  64, useBN=useBN)
+    self.conv1m = add_conv_stage( 64,  32, useBN=useBN)
 
-        self.Up4 = up_conv(ch_in=512,ch_out=256)
-        self.Up_conv4 = conv_block(ch_in=512, ch_out=256)
-        
-        self.Up3 = up_conv(ch_in=256,ch_out=128)
-        self.Up_conv3 = conv_block(ch_in=256, ch_out=128)
-        
-        self.Up2 = up_conv(ch_in=128,ch_out=64)
-        self.Up_conv2 = conv_block(ch_in=128, ch_out=64)
+    self.conv0  = nn.Sequential(
+        nn.Conv2d(32, 1, 3, 1, 1),
+        nn.Sigmoid()
+    )
 
-        self.Conv_1x1 = nn.Conv2d(64,output_ch,kernel_size=1,stride=1,padding=0)
+    self.max_pool = nn.MaxPool2d(2)
 
+    self.upsample54 = upsample(512, 256)
+    self.upsample43 = upsample(256, 128)
+    self.upsample32 = upsample(128,  64)
+    self.upsample21 = upsample(64 ,  32)
 
-    def forward(self,x):
-        # encoding path
-        print(x.size())
-        x1 = self.Conv1(x)
-        print(x1.size())
-        x2 = self.Maxpool(x1)
-        x2 = self.Conv2(x2)
-        print(x2.size())
-        x3 = self.Maxpool(x2)
-        x3 = self.Conv3(x3)
-        print(x3.size())
-        x4 = self.Maxpool(x3)
-        x4 = self.Conv4(x4)
-        print(x4.size())
-        x5 = self.Maxpool(x4)
-        x5 = self.Conv5(x5)
-        print(x5.size())
-        # decoding + concat path
-        d5 = self.Up5(x5)
-        d5 = torch.cat((x4,d5),dim=1)
-        
-        d5 = self.Up_conv5(d5)
-        
-        d4 = self.Up4(d5)
-        d4 = torch.cat((x3,d4),dim=1)
-        d4 = self.Up_conv4(d4)
-
-        d3 = self.Up3(d4)
-        d3 = torch.cat((x2,d3),dim=1)
-        d3 = self.Up_conv3(d3)
-
-        d2 = self.Up2(d3)
-        d2 = torch.cat((x1,d2),dim=1)
-        d2 = self.Up_conv2(d2)
-
-        d1 = self.Conv_1x1(d2)
-
-        return d1
-
-class conv_block(nn.Module):
-    def __init__(self,ch_in,ch_out):
-        super(conv_block,self).__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(ch_in, ch_out, kernel_size=3,stride=1,padding=1,bias=True),
-            nn.BatchNorm2d(ch_out),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(ch_out, ch_out, kernel_size=3,stride=1,padding=1,bias=True),
-            nn.BatchNorm2d(ch_out),
-            nn.ReLU(inplace=True)
-        )
+    ## weight initialization
+    for m in self.modules():
+      if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        if m.bias is not None:
+          m.bias.data.zero_()
 
 
-    def forward(self,x):
-        x = self.conv(x)
-        return x
+  def forward(self, x):
+    conv1_out = self.conv1(x)
+    #return self.upsample21(conv1_out)
+    conv2_out = self.conv2(self.max_pool(conv1_out))
+    conv3_out = self.conv3(self.max_pool(conv2_out))
+    conv4_out = self.conv4(self.max_pool(conv3_out))
+    conv5_out = self.conv5(self.max_pool(conv4_out))
 
-class up_conv(nn.Module):
-    def __init__(self,ch_in,ch_out):
-        super(up_conv,self).__init__()
-        self.up = nn.Sequential(
-            nn.Upsample(scale_factor=2),
-            nn.Conv2d(ch_in,ch_out,kernel_size=3,stride=1,padding=1,bias=True),
-		    nn.BatchNorm2d(ch_out),
-			nn.ReLU(inplace=True)
-        )
+    conv5m_out = torch.cat((self.upsample54(conv5_out), conv4_out), 1)
+    conv4m_out = self.conv4m(conv5m_out)
 
-    def forward(self,x):
-        x = self.up(x)
-        return x
+    conv4m_out_ = torch.cat((self.upsample43(conv4m_out), conv3_out), 1)
+    conv3m_out = self.conv3m(conv4m_out_)
 
+    conv3m_out_ = torch.cat((self.upsample32(conv3m_out), conv2_out), 1)
+    conv2m_out = self.conv2m(conv3m_out_)
+
+    conv2m_out_ = torch.cat((self.upsample21(conv2m_out), conv1_out), 1)
+    conv1m_out = self.conv1m(conv2m_out_)
+
+    conv0_out = self.conv0(conv1m_out)
+
+    return conv0_out
